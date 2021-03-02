1e32aebb5d20d004585f03646e81f2fb67decf96
mwritter <mritter2283@gmail.com>
2021-03-02 07:00:48 -0500

    Added checkout page checkout item component and update cart to use selectors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </React.StrictMode>,
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
